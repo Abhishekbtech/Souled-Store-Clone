139c40c700cfdd558580bff2b8747fc857ed901d
Abhishek Chauhan <abhishekb.tech271199@gmail.com>
2024-07-01 22:59:54 +0530

    update

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -6,25 +6,29 @@ import BestDeals from './BestDeals';
 // import Footer from '../components/Footer';
 
 const Home = () => {
-  return (
-    <div>
-      <HeroBanner />
-      {/* <Categories /> */}
-      <ProductsByCategory category="hoodie" />
-      <ProductsByCategory category="jeans" />
-      <ProductsByCategory category="jogger" />
-      <ProductsByCategory category="jumpsuit" />
-      <ProductsByCategory category="kurta" />
-      <ProductsByCategory category="kurti" />
-      <ProductsByCategory category="pyjamas" />
-      <ProductsByCategory category="shirt" />
-      <ProductsByCategory category="tshirt" />
-      
-      <BestDeals />
-      {/* <Footer /> */}
-      ji
-    </div>
-  );
+    return (
+        <div>
+            <HeroBanner />
+            {/* <Categories /> */}
+            <ProductsByCategory category="hoodie" />
+            <ProductsByCategory category="jeans" />
+            <ProductsByCategory category="jogger" />
+            <ProductsByCategory category="jumpsuit" />
+            <ProductsByCategory category="kurta" />
+            <ProductsByCategory category="kurti" />
+            <ProductsByCategory category="pyjamas" />
+            <ProductsByCategory category="shirt" />
+            <ProductsByCategory category="shorts" />
+            <ProductsByCategory category="sweater" />
+            <ProductsByCategory category="tracksuit" />
+            <ProductsByCategory category="trouser" />
+            <ProductsByCategory category="tshirt" />
+
+            <BestDeals />
+            {/* <Footer /> */}
+            ji
+        </div>
+    );
 };
 
 export default Home;
