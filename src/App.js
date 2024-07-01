b5123256b4bc3b83f67844dd318caba4c7970034
Abhishek Chauhan <abhishekb.tech271199@gmail.com>
2024-07-01 10:41:30 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import SecondNavbar from './components/SecondNavbar';
+import Navbar from './components/Navbar/NavBar';
+import SecondNavbar from './components/Navbar/SecondNavbar';
 import Home from './components/HomePage/HomePage';
 import Men from './components/MenPage/MenPage';
 import Women from './components/WomenPage/WomenPage';
