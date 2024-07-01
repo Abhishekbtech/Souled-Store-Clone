3e3f7451b8235cff9d203f3e21c890d9204f7eab
Abhishek Chauhan <abhishekb.tech271199@gmail.com>
2024-07-01 10:30:23 +0530

    update NavBar

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -1,14 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { AppBar, Toolbar, Typography, Button, IconButton, Menu, MenuItem } from '@mui/material';
 import { Link } from 'react-router-dom';
-import { Menu, MenuItem } from '@mui/material';
-import { Search, AccountCircle, ShoppingCart } from '@mui/icons-material';
-import logo from '../Image/Logo.png';
-// import CategoryDropdown from './CategoryDropdown';
+import MenuIcon from '@mui/icons-material/Menu';
 
-function NavBar() {
-    const [anchorEl, setAnchorEl] = useState(null);
+const NavBar = () => {
+    const [anchorEl, setAnchorEl] = React.useState(null);
 
-    const handleProfileMenuOpen = (event) => {
+    const handleMenuOpen = (event) => {
         setAnchorEl(event.currentTarget);
     };
 
@@ -16,55 +14,44 @@ function NavBar() {
         setAnchorEl(null);
     };
 
-    const isMenuOpen = Boolean(anchorEl);
-
-    const renderMenu = (
-        <Menu
-            anchorEl={anchorEl}
-            anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-            keepMounted
-            transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-            open={isMenuOpen}
-            onClose={handleMenuClose}
-        >
-            <MenuItem onClick={handleMenuClose}>My Wishlist</MenuItem>
-            <MenuItem onClick={handleMenuClose}>My Orders</MenuItem>
-            <MenuItem onClick={handleMenuClose}>Logout</MenuItem>
-        </Menu>
-    );
-
     return (
-        <div>
-            <nav className="bg-red-600 p-4">
-                <div className="container mx-auto flex items-center justify-between">
-                    <img src={logo} alt="The Souled Store" className="h-10" />
-                    <div className="flex space-x-4">
-                        {/* <CategoryDropdown gender="Women" />
-                        <CategoryDropdown gender="Men" /> */}
-                        <Link to="/women" className="text-white text-xl">WOMEN</Link>
-                        <Link to="/" className="text-white text-xl">MEN</Link>
-                        <Link to="/kids" className="text-white text-xl">KIDS</Link>
-                    </div>
-                    <div className="flex space-x-6 items-center">
-                        <Link to="#" className="text-white">Track Order</Link>
-                        <Link to="#" className="text-white">Contact Us</Link>
-                        <Link to="#" className="text-white">Download App</Link>
-                        <div className="relative">
-                            <input
-                                type="text"
-                                placeholder="Search…"
-                                className="px-4 py-2 rounded-full focus:outline-none"
-                            />
-                            <Search className="absolute right-2 top-1/2 transform -translate-y-1/2 text-gray-600" />
-                        </div>
-                        <ShoppingCart className="text-white" />
-                        <AccountCircle className="text-white cursor-pointer" onClick={handleProfileMenuOpen} />
-                    </div>
-                </div>
-            </nav>
-            {renderMenu}
-        </div>
+        <AppBar position="static">
+            <Toolbar>
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                    <Link to="/">The Souled Store</Link>
+                </Typography>
+                <Button color="inherit"><Link to="/men">Men</Link></Button>
+                <Button color="inherit"><Link to="/women">Women</Link></Button>
+                <IconButton
+                    size="large"
+                    edge="end"
+                    color="inherit"
+                    aria-label="menu"
+                    onClick={handleMenuOpen}
+                >
+                    <MenuIcon />
+                </IconButton>
+                <Menu
+                    anchorEl={anchorEl}
+                    anchorOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                    }}
+                    open={Boolean(anchorEl)}
+                    onClose={handleMenuClose}
+                >
+                    <MenuItem onClick={handleMenuClose}>My Wishlist</MenuItem>
+                    <MenuItem onClick={handleMenuClose}>My Orders</MenuItem>
+                    <MenuItem onClick={handleMenuClose}>Logout</MenuItem>
+                </Menu>
+            </Toolbar>
+        </AppBar>
     );
-}
+};
 
 export default NavBar;
