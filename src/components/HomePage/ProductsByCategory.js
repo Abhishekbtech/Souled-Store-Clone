1f0add8bc0ba650c74fb1a7a6f5ed3f87afb1588
Abhishek Chauhan <abhishekb.tech271199@gmail.com>
2024-07-01 22:23:55 +0530

    update

--- src/components/HomePage/ProductsByCategory.js
+++ src/components/HomePage/ProductsByCategory.js
@@ -35,7 +35,7 @@ const ProductsByCategory = ({ category }) => {
             <div className="container mx-auto">
                 <Slider {...settings}>
                     {products.map((product) => (
-                        <div key={product.id} className="product-card bg-white p-2 rounded-lg shadow-md">
+                        <div key={product.id} className="product-card bg-white p-4 rounded-lg shadow-md mx-2 max-w-xs">
                             <img src={product.displayImage} alt={product.name} className="h-35 w-full object-cover rounded-md mb-2" />
                             <h3 className="text-lg font-semibold">{product.name}</h3>
                             <p className="text-gray-700">${product.price}</p>
