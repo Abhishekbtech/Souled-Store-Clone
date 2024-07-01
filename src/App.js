1d9a703543168dde0c6ceee1cc1e39f9f6834482
Abhishek Chauhan <abhishekb.tech271199@gmail.com>
2024-07-01 10:34:51 +0530

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import MenPage from './components/MenPage/MenPage';
-import WomenPage from './components/WomenPage/WomenPage';
-import NavBar from './components/Navbar/NavBar';
-// import CategoryDropdown from './components/Navbar/CategoryDropdown';
-// import Footer from './components/Footer/Footer';
+import Navbar from './components/Navbar';
+import SecondNavbar from './components/SecondNavbar';
+import Home from './components/HomePage/HomePage';
+import Men from './components/MenPage/MenPage';
+import Women from './components/WomenPage/WomenPage';
 
 function App() {
   return (
     <Router>
-      <NavBar />
-      {/* <CategoryDropdown/> */}
-      <Routes>
-        <Route path="/" element={<MenPage />} />
-        <Route path="/women" element={<WomenPage />} />
-      </Routes>
-      {/* <Footer/> */}
+      <div>
+        <Navbar />
+        <SecondNavbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/men" element={<Men />} />
+          <Route path="/women" element={<Women />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
