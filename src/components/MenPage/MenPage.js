cae64f38ed83072301c77f37957bf3231b5f0b39
Abhishek Chauhan <abhishekb.tech271199@gmail.com>
2024-07-04 13:20:40 +0530

    add product

--- src/components/MenPage/MenPage.js
+++ src/components/MenPage/MenPage.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import ProductsByCategory from '../HomePage/ProductsByCategory'
 
 function MenPage() {
     return (
-        <h1>Men's Collection</h1>
+        <>
+            <ProductsByCategory category="hoodie" />
+            <ProductsByCategory category="pyjamas" />
+            <ProductsByCategory category="shirt" />
+            <ProductsByCategory category="shorts" />
+            <ProductsByCategory category="sweater" />
+            <ProductsByCategory category="tracksuit" />
+            <ProductsByCategory category="trouser" />
+            <ProductsByCategory category="tshirt" />
+        </>
     )
 }
 
