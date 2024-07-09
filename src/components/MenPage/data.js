54a9d43e9e19b3a3b864d01c40bc20073d895f35
Abhishek Chauhan <abhishekb.tech271199@gmail.com>
2024-07-09 22:18:16 +0530

    add

---
+++ src/components/MenPage/data.js
@@ -0,0 +1,13 @@
+const categories = [
+    {id: 1, url: "https://prod-img.thesouledstore.com/public/theSoul/storage/mobile-cms-media-prod/product-images/3_23_CgLv44W.jpg?format=webp&w=480&dpr=1.3", href: "jeans", title: "jeans", gender: "Men"},
+    {id: 2, url: "https://prod-img.thesouledstore.com/public/theSoul/storage/mobile-cms-media-prod/product-images/2_22_IWKyEFx.jpg?format=webp&w=480&dpr=1.3", href: "shirts", title: "shirt", gender: "Men"},
+    {id: 3, url: "https://prod-img.thesouledstore.com/public/theSoul/storage/mobile-cms-media-prod/product-images/7_16_tROGkdp.jpg?format=webp&w=480&dpr=1.3", href: "tshirts", title: "tshirt",gender: "Men" },
+    {id: 4, url: "https://prod-img.thesouledstore.com/public/theSoul/storage/mobile-cms-media-prod/product-images/t7_jKnDqWg.jpg?format=webp&w=480&dpr=1.3", href: "hoodie", title: "hoodie", gender: "Men" },
+    {id: 5, url: "https://prod-img.thesouledstore.com/public/theSoul/storage/mobile-cms-media-prod/product-images/9_10_nR3LyYP.jpg?format=webp&w=480&dpr=1.3", href: "joggers", title: "jogger", gender: "Men"},
+    {id: 6, url: "https://prod-img.thesouledstore.com/public/theSoul/storage/mobile-cms-media-prod/product-images/11_2_U7gKF3z.jpg?format=webp&w=480&dpr=1.3", href: "shorts", title: "shorts", gender: "Men"},
+    {id: 7, url: "https://prod-img.thesouledstore.com/public/theSoul/storage/mobile-cms-media-prod/product-images/Small-Tile-313_ywA7XBr.jpg?format=webp&w=480&dpr=1.3", href: "sweater", title: "sweater",gender: "Men"},
+    {id: 8, url: "https://prod-img.thesouledstore.com/public/theSoul/storage/mobile-cms-media-prod/product-images/4_19_1BR4FHS.jpg?format=webp&w=480&dpr=1.3", href: "trouser", title: "trouser",gender: "Men"},
+    {id: 9, url: "https://images.bewakoof.com/uploads/grid/app/TrendingCategory-Carousel-600x750-new-Men-8-1717767104.jpg", href: "trouser", title: "pyjamas",gender: "Men"}
+];
+
+export default categories;
