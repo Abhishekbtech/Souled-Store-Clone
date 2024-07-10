cb330ad4976f7103b42c2e8588ae5fa4fdfb7a56
Abhishek Chauhan <abhishekb.tech271199@gmail.com>
2024-07-10 11:04:30 +0530

    add

---
+++ src/components/WomenPage/data.js
@@ -0,0 +1,10 @@
+const categories = [
+    {id: 1, url: "https://prod-img.thesouledstore.com/public/theSoul/storage/mobile-cms-media-prod/product-images/Big-Tile-1_05_zuB2AXB.jpg?format=webp&w=480&dpr=1.3", href: "jeans", title: "jeans", gender: "Women"},
+    {id: 2, url: "https://prod-img.thesouledstore.com/public/theSoul/storage/mobile-cms-media-prod/product-images/Big-Tile-1_04_h7OJqWw.jpg?format=webp&w=480&dpr=1.3", href: "shirts", title: "shirt", gender: "Women"},
+    {id: 3, url: "https://prod-img.thesouledstore.com/public/theSoul/storage/mobile-cms-media-prod/product-images/Big-Tile-1_09_wDfeoXr.jpg?format=webp&w=480&dpr=1.3", href: "tshirts", title: "tshirt",gender: "Women" },
+    {id: 4, url: "https://prod-img.thesouledstore.com/public/theSoul/storage/mobile-cms-media-prod/product-images/Big-Tile-1_08_80VAAlZ.jpg?format=webp&w=480&dpr=1.3", href: "joggers", title: "jogger", gender: "Women"},
+    {id: 5, url: "https://prod-img.thesouledstore.com/public/theSoul/storage/mobile-cms-media-prod/product-images/Big-Tile-1_02_3NB2I7d.jpg?format=webp&w=480&dpr=1.3", href: "joggers", title: "jumpsuit", gender: "Women"},
+    {id: 6, url: "https://prod-img.thesouledstore.com/public/theSoul/storage/mobile-cms-media-prod/product-images/Big-Tile-1_03_AL6Vs0z.jpg?format=webp&w=480&dpr=1.3", href: "joggers", title: "kurti", gender: "Women"},
+];
+
+export default categories;
