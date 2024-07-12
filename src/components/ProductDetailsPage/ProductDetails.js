4cc25a5edf63c5c2b7e345397e7af3ca8cf21f55
Abhishek Chauhan <abhishekb.tech271199@gmail.com>
2024-07-12 12:01:07 +0530

    add m

--- src/components/ProductDetailsPage/ProductDetails.js
+++ src/components/ProductDetailsPage/ProductDetails.js
@@ -62,7 +62,7 @@ const ProductDetails = () => {
                     <h1 className="text-2xl font-bold mb-3">{product.name}</h1>
                     <hr/>
                     <p className="text-xl text-gray-800 mt-4">₹{product.price}</p>
-                    <p>Inclusive of all taxes</p>
+                    <p className='text-sm mt-1'>MRP incl. of all taxes</p>
                     <div className="mb-4">
                         <h3 className="font-semibold mb-2">Please select a size: <span className="text-blue-500 cursor-pointer">SIZE CHART</span></h3>
                         <div className="flex space-x-2">
