2a3b0403f9c2324c5a2ae5be137e1e040b9424e4
Abhishek Chauhan <abhishekb.tech271199@gmail.com>
2024-07-13 23:02:44 +0530

    add

--- src/components/MenPage/ScorlPage.js
+++ src/components/MenPage/ScorlPage.js
@@ -1,20 +1,20 @@
 import React from 'react'
-import { Sales } from '../data/data';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import { Link } from 'react-router-dom';
+import ScorlData from './data/ScorlData';
 
 function ScorlPage() {
     const responsive = {
         desktop: {
             breakpoint: { max: 3000, min: 1024 },
-            items: 3,
-            slidesToSlide: 2
+            items: 1,
+            slidesToSlide: 1
         },
         tablet: {
             breakpoint: { max: 1024, min: 464 },
-            items: 3,
-            slidesToSlide: 2
+            items: 1,
+            slidesToSlide: 1
         },
         mobile: {
             breakpoint: { max: 464, min: 0 },
@@ -31,13 +31,13 @@ function ScorlPage() {
             infinite={true}
             showDots={true}
             autoPlay={true}
-            autoPlaySpeed={4000}
+            autoPlaySpeed={3000}
             dotListClass="custom-dot-list-style"
             itemClass="carousel-item-padding-40-px"
         >
-            {Sales.map((item) => (
+            {ScorlData.map((item) => (
                 <div key={item.id}>
-                    <Link to={`/${item.href}`} className="swiper-slide" data-swiper-slide-index={`${item.index}`}>
+                    <Link to={`/men/${item.title}`} className="swiper-slide" data-swiper-slide-index={`${item.index}`}>
                         <img src={`${item.url}`} className='swiper-slide-img' alt='title' />
                     </Link>
                 </div>
