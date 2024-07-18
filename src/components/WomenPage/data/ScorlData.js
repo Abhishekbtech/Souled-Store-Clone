29dbe00b911f2a23c78a0620d36fced7d66f2689
Abhishek Chauhan <abhishekb.tech271199@gmail.com>
2024-07-18 23:09:56 +0530

    add

---
+++ src/components/WomenPage/data/ScorlData.js
@@ -0,0 +1,7 @@
+const ScorlData = [
+    {id: 1, url: "https://prod-img.thesouledstore.com/public/theSoul/storage/mobile-cms-media-prod/banner-images/homepage_banner_copy_2_dN8S3HR.jpg?format=webp&w=1500&dpr=1.3", title: "shirt", gender: "Women"},
+    {id: 2, url: "https://prod-img.thesouledstore.com/public/theSoul/storage/mobile-cms-media-prod/banner-images/home_banner_.jpg?format=webp&w=1500&dpr=1.3", href: "trouser", title: "jeans",gender: "Women"},
+    {id: 3, url: "https://prod-img.thesouledstore.com/public/theSoul/storage/mobile-cms-media-prod/banner-images/OS_homepage_banner_copy.jpg?format=webp&w=1500&dpr=1.3", title: "tshirt", gender: "Women"},
+]
+
+export default ScorlData
