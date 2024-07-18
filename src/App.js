5a34c18bb2cdf2dd1d9dd65de56ab12bf21a1805
Abhishek Chauhan <abhishekb.tech271199@gmail.com>
2024-07-18 23:20:12 +0530

    add

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar/NavBar';
 import SecondNavbar from './components/Navbar/SecondNavbar';
-import Home from './components/HomePage/HomePage';
 import Men from './components/MenPage/MenPage';
 import Women from './components/WomenPage/WomenPage';
 import SearchPage from './components/Search/SearchPage';
@@ -26,7 +25,6 @@ function App() {
         <Navbar />
         <SecondNavbar />
         <Routes>
-          {/* <Route path="/" element={<Home />} /> */}
           <Route path="/" element={<Men />} />
           <Route path="/women" element={<Women />} />
           <Route path='/search' element={<SearchPage/>}/>
