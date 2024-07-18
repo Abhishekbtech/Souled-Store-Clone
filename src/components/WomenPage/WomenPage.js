1c7241f3b2efc5f3df15e24997b7cf03514754eb
Abhishek Chauhan <abhishekb.tech271199@gmail.com>
2024-07-18 23:13:09 +0530

    add

--- src/components/WomenPage/WomenPage.js
+++ src/components/WomenPage/WomenPage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import ProductsBySellerTag from '../CategoryData/ProductsBySellerTag'
 import Categorie from './Categorie'
+import ScrolPage from './ScrolPage'
 
 function WomenPage() {
     return (
         <div className='m-3'>
+            <ScrolPage/>
             <Categorie/>
             <ProductsBySellerTag gender="Women" sellerTag="trending"/>
             <ProductsBySellerTag gender="Women" sellerTag="top rated"/>
