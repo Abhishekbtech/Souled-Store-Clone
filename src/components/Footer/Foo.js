2f15991f056ef2b1cfefbd6df48774757b7055ed
Abhishek Chauhan <abhishekb.tech271199@gmail.com>
2024-07-19 23:10:00 +0530

    finsh footer componet

--- src/components/Footer/Foo.js
+++ src/components/Footer/Foo.js
@@ -8,9 +8,11 @@ function Foo() {
             <div className="bg-red-600 text-white text-center py-4">
                 <p className="text-3xl font-semibold">HOMEGROWN INDIAN BRAND</p>
             </div>
+            
             <div className="bg-white text-center py-4">
                 <p className="text-4xl font-semibold">Over <span className="text-4xl font-bold">6 Million</span> Happy Customers</p>
             </div>
+
             <div className='mx-2 sm:mx-4 md:mx-10 lg:mx-20'>
                 <div className="grid grid-cols-2 sm:grid-cols-1 md:grid-cols-4 lg:grid-cols-4 gap-4 p-4">
                     <div className="mx-4 my-5">
@@ -23,6 +25,7 @@ function Foo() {
                             <li>My Account</li>
                         </ul>
                     </div>
+
                     <div className="mx-4 my-5">
                         <h2 className="text-xl font-bold text-red-600">COMPANY</h2>
                         <ul className="mt-4 font-bold">
@@ -32,6 +35,7 @@ function Foo() {
                             <li>Souled Army</li>
                         </ul>
                     </div>
+
                     <div className="mx-4 my-5">
                         <h2 className="text-xl font-bold text-red-600">MORE INFO</h2>
                         <ul className="mt-4 font-bold">
@@ -40,6 +44,7 @@ function Foo() {
                             <li>Sitemap</li>
                         </ul>
                     </div>
+
                     <div className="mx-4 my-5">
                         <h2 className="text-xl font-bold text-red-600">STORE NEAR ME</h2>
                         <ul className="mt-4 font-bold">
@@ -51,12 +56,14 @@ function Foo() {
                         </ul>
                     </div>
                 </div>
+
                 <div className='flex justify-start'>
                     <div className=" ml-10">
                         <p>₹ COD Available</p>
                         <p>↩ 30 Days Easy Returns</p>
                     </div>
                 </div>
+
                 <div className="flex justify-end mt-5">
                     <div className="container mx-auto px-5 text-center">
                         <p className="mb-2">📱 EXPERIENCE THE SOULED STORE APP</p>
@@ -66,6 +73,7 @@ function Foo() {
                         </div>
                     </div>
                 </div>
+
                 <div className="flex justify-end my-5 mr-10">
                     <div className="flex items-center space-x-4">
                         <h3 className='mt-1'>Follow us :</h3>
@@ -75,6 +83,7 @@ function Foo() {
                         <a href="https://twitter.com/TheSouledStore" className="text-2xl text-blue-500"><FontAwesomeIcon icon={faTwitter} /></a>
                     </div>
                 </div>
+
                 <div className='text-center pb-10'>
                     <h1 className='font-semibold text-xl'>© The Souled Store 2024-25</h1>
                 </div>
