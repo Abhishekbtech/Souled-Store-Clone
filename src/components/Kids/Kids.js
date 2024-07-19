831cff59da9f2670895571159333fc09823229ea
Abhishek Chauhan <abhishekb.tech271199@gmail.com>
2024-07-19 21:43:54 +0530

    add

---
+++ src/components/Kids/Kids.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Kids() {
+    return (
+        <div className='text-center my-20'>
+            <h1 className='font-bold text-2xl'>Comming Soon .....</h1>
+        </div>
+    )
+}
+
+export default Kids
