22c0e3da4cbe3d3339f395c99e663023a70a1dc8
Abhishek Chauhan <abhishekb.tech271199@gmail.com>
2024-07-19 12:54:36 +0530

    add

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -1,7 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const Navbar = () => {
+    const location = useLocation();
+    const isMenPage = location.pathname === '/';
+    const isWomenPage = location.pathname === '/women';
+    const isKidsPage = location.pathname === '/kids';
+
     return (
         <nav className="bg-red-600 p-4">
             <div className="container mx-auto flex items-center justify-between">
@@ -9,9 +14,24 @@ const Navbar = () => {
                     <img src="/path/to/logo.png" alt="The Souled Store" className="h-8" />
                 </Link>
                 <div className="hidden md:flex space-x-4">
-                    <Link to="/women" className="text-white">Women</Link>
-                    <Link to="/" className="text-white">Men</Link>
-                    <Link to="/kids" className="text-white">Kids</Link>
+                    <Link 
+                        to="/women" 
+                        className={`text-xl ${isWomenPage ? 'bg-white text-red-600' : 'text-white'}`}
+                    >
+                        Women
+                    </Link>
+                    <Link 
+                        to="/" 
+                        className={`text-xl ${isMenPage ? 'bg-white text-red-600' : 'text-white'}`}
+                    >
+                        Men
+                    </Link>
+                    <Link 
+                        to="/kids" 
+                        className={`text-xl ${isKidsPage ? 'bg-white text-red-600' : 'text-white'}`}
+                    >
+                        Kids
+                    </Link>
                 </div>
                 <div className="flex items-center space-x-4">
                     <button className="text-white hidden md:block" onClick={() => alert('Track Order')}>Track Order</button>
