1ddba834e43f67836b34634490156f1b481bbfdc
Abhishek Chauhan <abhishekb.tech271199@gmail.com>
2024-08-06 21:43:51 +0530

    remove

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -13,28 +13,28 @@ const Navbar = () => {
                 <div className="flex space-x-4">
                     <Link 
                         to="/women" 
-                        className={`text-xl px-2 ${isWomenPage ? 'bg-white text-red-600' : 'text-white'}`}
+                        className={`text-xl px-2 ${isWomenPage ? 'bg-white rounded-md text-red-600' : 'text-white'}`}
                     >
                         Women
                     </Link>
                     <Link 
                         to="/" 
-                        className={`text-xl px-2 ${isMenPage ? 'bg-white text-red-600' : 'text-white'}`}
+                        className={`text-xl px-2 ${isMenPage ? 'bg-white rounded-md text-red-600' : 'text-white'}`}
                     >
                         Men
                     </Link>
                     <Link 
                         to="/kids" 
-                        className={`text-xl px-2 ${isKidsPage ? 'bg-white text-red-600' : 'text-white'}`}
+                        className={`text-xl px-2 ${isKidsPage ? 'bg-white rounded-md text-red-600' : 'text-white'}`}
                     >
                         Kids
                     </Link>
                 </div>
-                <div className="items-center space-x-4 hidden sm:flex">
+                {/* <div className="items-center space-x-4 hidden sm:flex">
                     <button className="text-white" onClick={() => alert('Track Order')}>Track Order</button>
                     <button className="text-white" onClick={() => alert('Contact Us')}>Contact Us</button>
                     <button className="text-white" onClick={() => alert('Download App')}>Download App</button>
-                </div>
+                </div> */}
             </div>
         </nav>
     );
